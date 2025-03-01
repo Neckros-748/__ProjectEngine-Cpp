a0392b212caf95c9efd4591119fa9f873c251860
Бронников Алексей <alexbronnikov34@gmail.com>
2025-03-01 23:37:37 +1000

    Create Project: 'ProjectEngine-Cpp', version - V:0.0.1 / 000

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,189 @@
-int main(int argc, char** argv)
+#include <iostream>
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+
+// Размеры окна
+const unsigned int SCR_WIDTH  = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// Вершинный шейдер
+const char* vertex_shader_source = R"(
+#version 330 core
+layout (location = 0) in vec3 pos;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+void main()
 {
+    gl_Position = projection * view * model * vec4(pos, 1.0);
+}
+)";
+
+// Фрагментный шейдер
+const char* fragment_shader_source = R"(
+#version 330 core
+out vec4 FragColor;
+void main()
+{
+    FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+}
+)";
+
+
+
+// Функция для компиляции шейдеров
+unsigned int compileShader(unsigned int type, const char* source) {
+    unsigned int shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
+
+    int success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char info_log[512];
+        glGetShaderInfoLog(shader, 512, nullptr, info_log);
+        glDeleteShader(shader);
+        std::cerr << "ERROR::SHADER::COMPILATION_FAILED\n" << info_log << std::endl;
+    }
+    return shader;
+}
+
+// Функция для создания шейдерной программы
+unsigned int createShaderProgram(const char* vertex_source, const char* fragment_source) {
+    unsigned int vertex_shader   = compileShader(GL_VERTEX_SHADER, vertex_source);
+    unsigned int fragment_shader = compileShader(GL_FRAGMENT_SHADER, fragment_source);
+
+    unsigned int shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    int success;
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success) {
+        char info_log[512];
+        glGetProgramInfoLog(shader_program, 512, nullptr, info_log);
+        glDeleteProgram(shader_program);
+        std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << info_log << std::endl;
+    }
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    return shader_program;
+}
+
+
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Voxel Engine", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Создание шейдерной программы
+    unsigned int shader_program = createShaderProgram(vertex_shader_source, fragment_shader_source);
+
+    // Вершины куба
+    float vertices[] = {
+        // Передняя грань
+        -0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        // Задняя грань
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f
+    };
+
+    // Индексы для отрисовки куба
+    unsigned int indices[] = {
+        // Передняя грань
+        0, 1, 2,
+        2, 3, 0,
+        // Задняя грань
+        4, 5, 6,
+        6, 7, 4,
+    };
+
+    // Создание VAO, VBO и EBO
+    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // Настройка матриц
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+
+    // Основной цикл рендеринга
+    while (!glfwWindowShouldClose(window)) {
+        // Очистка экрана
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Использование шейдерной программы
+        glUseProgram(shader_program);
+
+        // Передача матриц в шейдер
+        glUniformMatrix4fv(glGetUniformLocation(shader_program, "model"), 1, GL_FALSE, &model[0][0]);
+        glUniformMatrix4fv(glGetUniformLocation(shader_program, "view"), 1, GL_FALSE, &view[0][0]);
+        glUniformMatrix4fv(glGetUniformLocation(shader_program, "projection"), 1, GL_FALSE, &projection[0][0]);
+
+        // Отрисовка куба
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+        // Обмен буферов и обработка событий
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Освобождение ресурсов
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shader_program);
+
+    // Завершение работы GLFW
+    glfwTerminate();
     return 0;
 }
